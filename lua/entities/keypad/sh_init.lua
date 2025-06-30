4a91f1e120893c1f7ea7bd3ba643a7f0bb3e6e78
thecraftianman <64441307+thecraftianman@users.noreply.github.com>
2025-06-30 16:40:01 -0400

    Try to remove keypads from the spawnmenu
    I've had some issues getting this sort of thing to work in the past, but hopefully this should just remove the keypad entity from the spawnmenu while still allowing it to be duplicated and everything.

--- lua/entities/keypad/sh_init.lua
+++ lua/entities/keypad/sh_init.lua
@@ -2,7 +2,7 @@ ENT.Base = WireLib and "base_wire_entity" or "base_gmodentity"
 ENT.Type = "anim"
 
 ENT.Model = Model("models/props_lab/keypad.mdl")
-ENT.Spawnable = true
+ENT.Spawnable = false
 
 ENT.Scale = 0.02
 ENT.Value = ""
@@ -19,6 +19,9 @@ ENT.IsKeypad = true
 
 AccessorFunc(ENT, "m_Password", "Password", FORCE_STRING)
 AccessorFunc(ENT, "m_KeypadOwner", "KeypadOwner")
+
+duplicator.RegisterEntityClass("keypad", duplicator.GenericDuplicatorFunction, "Data")
+duplicator.RegisterEntityClass("keypad_wire", duplicator.GenericDuplicatorFunction, "Data")
 
 function ENT:Initialize()
 	self:SetModel(self.Model)
